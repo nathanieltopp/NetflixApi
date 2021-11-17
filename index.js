21da8fde1204de76d37f9913264201da05b62d4c
Nath <yankahnathanieltopp@gmail.com>
2021-11-17 12:07:35 +0000

    authentication router completed

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+const authRoute = require("./routes/auth");
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
     .then(()=>console.log("DB connection succesfull!"))
     .catch(err => console.log(err));
+
+//setting up the json parser middleware
+app.use(express.json());
+
+//Configuring routes
+app.use("/api/auth", authRoute);
 
 
 app.listen(8800, () => {
